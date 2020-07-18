b56a66bd2fb31a0253825456af2e76d916f7629d
Jeferson <jeferson.hsa@gmail.com>
2020-07-18 01:56:08 -0300

    adds arg validation to details.go

--- cmd/details.go
+++ cmd/details.go
@@ -1,19 +1,28 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
 	"mongo-transfer/database"
 )
 
-var listDatabasesCmd = &cobra.Command{
+var detailsCmd = &cobra.Command{
 	Use:   "details",
 	Short: "",
 	Long:  ``,
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		if args[0] == "" {
+			return errors.New("not a valid server ID")
+		}
+
+		serverID := args[0]
+
+		fmt.Printf("Fetching details for \"%s\"\n", serverID)
+
 		server := database.Server{}
 
-		err = server.Load("local")
+		err = server.Load(serverID)
 		if err != nil {
 			return
 		}
@@ -32,5 +41,5 @@ var listDatabasesCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(listDatabasesCmd)
+	rootCmd.AddCommand(detailsCmd)
 }
