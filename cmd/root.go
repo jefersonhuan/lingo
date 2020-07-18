eab8e7cf32b68d904464e79d2a52e225e90dabe2
Jeferson <jeferson.hsa@gmail.com>
2020-07-18 01:55:54 -0300

    cleans up default content from root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,21 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
 )
 
+var serverURIFlag, serverIDFlag string
+
+var ErrRecordNotFound = errors.New("Unable to find file or directory named servers/local")
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "mongo-transfer",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "",
+	Long:  ``,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -25,10 +25,4 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func init() {
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
