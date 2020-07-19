1b56a31af1852742a4e8674544a57d751aee7b76
Jeferson <jeferson.hsa@gmail.com>
2020-07-19 18:43:33 -0300

    rearranged utils files

--- utils/helpers.go
+++ utils/helpers.go
@@ -7,6 +7,39 @@ import (
 	"github.com/viant/toolbox"
 )
 
+func StepsFunctions(functions ...func() error) error {
+	for _, f := range functions {
+		err := f()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+var colors = map[string]string{
+	"reset": "\033[0m",
+
+	"red":    "\033[31m",
+	"green":  "\033[32m",
+	"yellow": "\033[33m",
+	"blue":   "\033[34m",
+	"purple": "\033[35m",
+	"cyan":   "\033[36m",
+	"white":  "\033[37m",
+}
+
+func ColorfulString(color string, content string) string {
+	code, ok := colors[color]
+
+	if !ok {
+		return content
+	} else {
+		return code + string(content) + colors["reset"]
+	}
+}
+
 var endlyManager = endly.New()
 var endlyContext = endlyManager.NewContext(toolbox.NewContext())
 
