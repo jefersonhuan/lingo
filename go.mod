29790255c6e1518e46aa81eca3878be97002b73a
Jeferson <jeferson.hsa@gmail.com>
2020-07-20 01:58:35 -0300

    removes endly

--- go.mod
+++ go.mod
@@ -9,9 +9,5 @@ require (
 	github.com/sdomino/scribble v0.0.0-20200707180004-3cc68461d505
 	github.com/spf13/cobra v1.0.0
 	github.com/vbauerster/mpb v3.4.0+incompatible
-	github.com/viant/endly v0.49.0
-	github.com/viant/toolbox v0.31.0
 	go.mongodb.org/mongo-driver v1.3.5
-	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
-	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 )
--- utils/helpers.go
+++ utils/helpers.go
@@ -1,12 +1,5 @@
 package utils
 
-import (
-	"fmt"
-	"github.com/viant/endly"
-	"github.com/viant/endly/system/docker"
-	"github.com/viant/toolbox"
-)
-
 var colors = map[string]string{
 	"reset": "\033[0m",
 
@@ -37,29 +30,5 @@ func ColorfulString(color string, content string) string {
 		return content
 	} else {
 		return code + string(content) + colors["reset"]
-	}
-}
-
-var endlyManager = endly.New()
-var endlyContext = endlyManager.NewContext(toolbox.NewContext())
-
-func StartMongo() error {
-	_, err := endlyManager.Run(endlyContext, &docker.RunRequest{
-		Image: "mongo:latest",
-		Ports: map[string]string{
-			"27017": "27017",
-		},
-		Name: "mongo",
-	})
-	return err
-}
-
-func StopMongo() {
-	_, err := endlyManager.Run(endlyContext, &docker.StopRequest{
-		Name: "mongo",
-	})
-
-	if err != nil {
-		fmt.Println("An error occurred while stopping MongoDB", err)
 	}
 }
